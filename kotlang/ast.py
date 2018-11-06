jstasiak/kotlang

commit f47fc29d3e1a611b6890ac4f51d636a42366ddb7

Import functions directly from C headers - first steps
The Kit language has this[1] and I believe it's a great feature. The
pycparser library that I initially wanted to use is no good here though,
as it can't parse standard library headers by default[2] and although I
really tried to make it work, a growing set of preprocessor defines ([3]
is just a start, I reached about 13 I believe) made me give up.

Fortunately I looked around and I found another blog post by Eli
Bendersky[4] on parsing C++ in Python with libclang. It turned out to be
relatively easy and libclang's Python bindings (vendored in this
project in an earlier commit) are a good tool for the task. After
reading some source code and looking at examples in the clang repository
(I'm not aware of any reasonable documentation) I got it working.

The scope of what's working is limited right now - I don't support
header-declared structs, typedefs, defines and variables at the moment
and I only import few selected functions (because other functions depend
on structs, typedefs etc.) but with some hacks sprinkled on top it's
working rather nicely and seamlessly. I expect to extend the integration
layer soon.

[1] https://www.kitlang.org/examples.html#c-interoperability
[2] https://eli.thegreenplace.net/2015/on-parsing-c-type-declarations-and-fake-headers/
[3] https://stackoverflow.com/questions/10353902/any-way-to-get-the-c-preproccessor-to-ignore-all-includes
[4] https://eli.thegreenplace.net/2011/07/03/parsing-c-in-python-with-clang/

kotlang/ast.py
@@ -242,19 +242,19 @@ def get_or_create_llvm_function(
             function.parameters.variadic,
         )
         llvm_function = ir.Function(module, function_type, name=symbol_name)
-        for p, arg in zip(function.parameters, llvm_function.args):
-            arg.name = p.name + '_arg'
+        for i, (p, arg) in enumerate(zip(function.parameters, llvm_function.args)):
+            arg.name = (p.name or f'param{i + 1}') + '_arg'
 
         if function.code_block is not None:
             block = llvm_function.append_basic_block(name="entry")
             builder = ir.IRBuilder(block)
 
             function_namespace = Namespace(parents=[namespace])
-            for p, arg in zip(function.parameters, llvm_function.args):
+            for i, (p, arg) in enumerate(zip(function.parameters, llvm_function.args)):
                 memory = builder.alloca(arg.type, name=p.name)
                 builder.store(arg, memory)
                 parameter_type = p.type_.codegen(namespace)
-                function_namespace.add_item(Variable(p.name, parameter_type, memory))
+                function_namespace.add_item(Variable(p.name or f'param{i}', parameter_type, memory))
 
             function.code_block.codegen(module, builder, function_namespace)
             if function.return_type == 'void':
@@ -297,11 +297,16 @@ class Module:
     ) -> 'Namespace':
         import_namespaces: List[Namespace] = []
         for name, i in self.imports:
-            if meta_namespace.has(name):
+            # FIXME we currently need to special-case the c module where we put bits
+            # imported from C headers. If we remove this special handling one set of headers imported by
+            # one module would overshadow another set of headers in another module processed later.
+            # This whole MetaNamespace hack needs to go most likely.
+            if meta_namespace.has(name) and name != 'c':
                 imported_namespace = meta_namespace.get(name)
             else:
                 imported_namespace = i.codegen(module, parent_namespace, meta_namespace)
-                meta_namespace.set(name, imported_namespace)
+                if name != 'c':
+                    meta_namespace.set(name, imported_namespace)
             import_namespaces.append(imported_namespace)
 
         module_namespace = Namespace(parents=[parent_namespace, *import_namespaces])
@@ -1001,7 +1006,7 @@ class PointerTypeReference(TypeReference):
 
 @dataclass
 class Parameter:
-    name: str
+    name: Optional[str]
     type_: TypeReference
 
 

.travis.yml
@@ -2,7 +2,17 @@ language: python
 matrix:
     fast_finish: true
     include:
-        - { python: "3.7", dist: xenial, sudo: true }
+      - python: "3.7"
+        dist: xenial
+        sudo: true
+        addons:
+          apt:
+            sources:
+              - ubuntu-toolchain-r-test
+              - sourceline: 'deb https://apt.llvm.org/xenial/ llvm-toolchain-xenial-7 main'
+                key_url: 'https://apt.llvm.org/llvm-snapshot.gpg.key'
+            packages:
+              - libclang-7-dev
 before_install:
   - sudo apt update; sudo apt install libsdl2-dev
 install: pip install '.[dev]'

kotlang/parser.py
@@ -1,10 +1,47 @@
 from collections import OrderedDict
 import os
+from pathlib import Path
+import platform
+import subprocess
 from typing import cast, Iterator, List, MutableMapping, Optional, Tuple, Union
 
 from kotlang import ast
 from kotlang.itertools import Peekable
 from kotlang.lexer import lex, Token, TokenType
+
+# Paths are hardcoded, can't be bothered to detect them at the moment
+if platform.system() == 'Linux':
+    # Ubuntu
+    libclang_file = '/usr/lib/llvm-7/lib/libclang.so'
+    include_paths = [
+        '/usr/local/include',
+        '/usr/local/clang-7.0.0/lib/clang/7.0.0/include',
+        '/usr/include/x86_64-linux-gnu',
+        '/usr/include',
+    ]
+else:
+    # Mac OS 10.13 with HomeBrew
+    libclang_file = '/usr/local/opt/llvm/lib/libclang.dylib'
+    include_paths = [
+        '/usr/local/include',
+        '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/10.0.0/include',  # noqa
+        '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include',
+        '/usr/include',
+        '/System/Library/Frameworks',
+        '/Library/Frameworks',
+    ]
+
+from kotlang.clang import cindex  # noqa
+cindex.Config.set_library_file(libclang_file)
+clang_index = cindex.Index.create()
+
+
+def find_header(header: str) -> str:
+    paths = [Path(ip) / header for ip in include_paths]
+    for p in paths:
+        if p.exists():
+            return str(p)
+    assert False, f'Header {header} not found'
 
 
 def parse(text: str, name: str) -> ast.Module:
@@ -71,16 +108,23 @@ def read_module(tokens: Peekable[Token]) -> ast.Module:
     functions = []
     structs = []
     imports = []
+    cfunctions = {}
     while tokens.peek().type is not TokenType.eof:
-        next_text = expect_no_eat(tokens, 'def', 'extern', 'struct', 'import').text
+        next_text = expect_no_eat(tokens, 'def', 'extern', 'struct', 'import', 'cimport').text
         if next_text == 'extern':
             functions.append(read_function_declaration(tokens))
         elif next_text == 'def':
             functions.append(read_function_definition(tokens))
         elif next_text == 'import':
             imports.append(read_import(tokens))
+        elif next_text == 'cimport':
+            cfunctions.update({f.name: f for f in read_cimport(tokens)})
         else:
             structs.append(read_struct_definition(tokens))
+
+    if cfunctions:
+        imports.append(('c', ast.Module([], list(cfunctions.values()), [])))
+
     return ast.Module(structs, functions, imports)
 
 
@@ -128,6 +172,68 @@ def read_import(tokens: Peekable[Token]) -> Tuple[str, ast.Module]:
     expect(tokens, ';')
     text = read_module_text(module_name)
     return (module_name, parse(text, module_name))
+
+
+def read_cimport(tokens: Peekable[Token]) -> List[ast.Function]:
+    # CImport = "cimport" StringLiteral ";";
+    expect(tokens, 'cimport')
+    header = expect(tokens, TokenType.string_literal).text
+    expect(tokens, ';')
+
+    # Stripping first and last quotation mark characters because that's what string literals have
+    header = header[1:-1]
+
+    tu = clang_index.parse(find_header(header))
+    functions: List[ast.Function] = []
+    cursors = [tu.cursor]
+    while cursors:
+        c = cursors.pop()
+        if (
+            c.kind is cindex.CursorKind.FUNCTION_DECL  # type: ignore
+            and not c.is_definition()
+            # TODO: This restriction is in place until we follow typedefs and import struct declarations.
+            # For now let's import few whitelisted functions.
+            and c.spelling in {'printf', 'abort', 'strlen'}
+        ):
+            functions.append(convert_c_function_declaration(c))
+        cursors.extend(c.get_children())
+
+    return functions
+
+
+def convert_c_function_declaration(declaration: cindex.Cursor) -> ast.Function:
+    return_type = convert_c_type_reference(declaration.type.get_result())
+
+    parameter_names_types = [
+        (p.spelling or None, convert_c_type_reference(p.type))
+        for p in declaration.get_arguments()
+    ]
+    parameters = ast.ParameterList(
+        [ast.Parameter(n, t) for (n, t) in parameter_names_types],
+        declaration.type.is_function_variadic(),
+    )
+    # TODO make Function take TypeReference as return_type and remove this return_type.name thing
+    assert isinstance(return_type, ast.BaseTypeReference)
+    return ast.Function(declaration.spelling, [], parameters, return_type.name, None, "c")
+
+
+def convert_c_type_reference(ref: cindex.Type) -> ast.TypeReference:
+    pointer_level = 0
+    while ref.kind is cindex.TypeKind.POINTER:  # type: ignore
+        pointer_level += 1
+        ref = ref.get_pointee()
+    t: ast.TypeReference = ast.BaseTypeReference(c_types_mapping[ref.kind])
+    for i in range(0, pointer_level):
+        t = t.as_pointer()
+    return t
+
+
+c_types_mapping = {
+    cindex.TypeKind.INT: 'i32',  # type: ignore
+    cindex.TypeKind.CHAR_S: 'i8',  # type: ignore
+    cindex.TypeKind.ULONG: 'u64',  # type: ignore
+    cindex.TypeKind.VOID: 'void',  # type: ignore
+}
 
 
 def read_module_text(name: str) -> str:

modules/libc.kot
@@ -1,3 +1,0 @@
-extern "c" def abort() -> void;
-extern "c" def printf(format: i8*, ...) -> i64;
-extern "c" def strlen(str: i8*) -> i64;

modules/std.kot
@@ -1,4 +1,5 @@
-import libc;
+cimport "stdio.h";
+cimport "stdlib.h";
 
 def assert(value: bool, message: i8*) -> void {
     if (not value) {

mypy.ini
@@ -4,3 +4,6 @@ follow_imports = error
 disallow_untyped_defs = true
 warn_no_return = true
 warn_redundant_casts = true
+
+[mypy-kotlang.clang.*]
+ignore_errors = True

setup.cfg
@@ -6,6 +6,7 @@ application-import-names = kotlang
 max-line-length = 115
 # Ignoring F821 because we use unquoted forward declarations in type hints (PEP 563)
 ignore = E701,F401,F821,W503
+exclude = kotlang/clang
 
 [tool:pytest]
 addopts = --cov-report term --cov-report html --cov-report xml --cov=kotlang --cov-branch

test_data/array.kot
@@ -1,4 +1,4 @@
-import libc;
+cimport "stdio.h";
 
 def sum(array: i64*, n: i64) -> i64 {
 	let result = 0;

commit a7ca76f1f1ad9956dfb3635a71e3bed8ed30aa5d

Stick to 1-based parameter numbering
We do i + 1 few lines above this so let's be consistent.

Fixes: f47fc29d3e1a ("Import functions directly from C headers - first steps")

kotlang/ast.py
@@ -254,7 +254,7 @@ def get_or_create_llvm_function(
                 memory = builder.alloca(arg.type, name=p.name)
                 builder.store(arg, memory)
                 parameter_type = p.type_.codegen(namespace)
-                function_namespace.add_item(Variable(p.name or f'param{i}', parameter_type, memory))
+                function_namespace.add_item(Variable(p.name or f'param{i + 1}', parameter_type, memory))
 
             function.code_block.codegen(module, builder, function_namespace)
             if function.return_type == 'void':