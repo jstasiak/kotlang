0e4688d9aa34925101aacd32a905a898e365da4e
Jakub Stasiak <jakub@stasiak.at>
2019-01-11 00:45:53 +0100

    Simplify name mangling
    Originally I wanted to follow the way C++ symbols are name-mangled but
    I don't think it's worth pursuing that goal. Instead I decoded to use
    the _kot_MODULE_SYMBOL format.

--- kotlang/symbols.py
+++ kotlang/symbols.py
@@ -7,7 +7,6 @@ LEGAL_SYMBOL_RE = re.compile('^[a-zA-Z_.$][a-zA-Z_.$0-9]*$')
 
 def mangle(path: Iterable[str]) -> str:
     assert type(path) is not str
-    prefixed_path = (f'{len(e)}{e}' for e in path)
-    mangled = ''.join(itertools.chain(['_ZN'], prefixed_path))
+    mangled = '_'.join(itertools.chain(['_kot'], path))
     assert LEGAL_SYMBOL_RE.match(mangled) is not None, mangled
     return mangled
