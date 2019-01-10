9394b7752466f4bb9fc0d35f1acb8aad6860eb73
Jakub Stasiak <jakub@stasiak.at>
2019-01-11 00:40:40 +0100

    Refactor name mangling
    Since we just assert that there are no invalid characters present in
    symbol parts we can do it just once at the end.

--- kotlang/symbols.py
+++ kotlang/symbols.py
@@ -7,11 +7,7 @@ LEGAL_SYMBOL_RE = re.compile('^[a-zA-Z_.$][a-zA-Z_.$0-9]*$')
 
 def mangle(path: Iterable[str]) -> str:
     assert type(path) is not str
-    sanitized_path = (sanitize(e) for e in path)
-    prefixed_path = (f'{len(e)}{e}' for e in sanitized_path)
-    return ''.join(itertools.chain(['_ZN'], prefixed_path))
-
-
-def sanitize(name: str) -> str:
-    assert LEGAL_SYMBOL_RE.match(name) is not None, name
-    return name
+    prefixed_path = (f'{len(e)}{e}' for e in path)
+    mangled = ''.join(itertools.chain(['_ZN'], prefixed_path))
+    assert LEGAL_SYMBOL_RE.match(mangled) is not None, mangled
+    return mangled
